cca12ea1059435f5534ab458b521bf7ff5f2b5ac
nahid940 <nahid940@gmail.com>
2020-03-28 18:34:18 +0600

    Changes added

--- hash-table.js
+++ hash-table.js
@@ -23,8 +23,10 @@ class HashTable{
     const address=this._hash(key)
     if(!this.data[address])
     {
+      // if there is no item in this address space then create a new space
       this.data[address]=[]
     }
+    //store the item in the space
     this.data[address].push([key,value])
 
     return this.data
@@ -51,10 +53,29 @@ class HashTable{
 }
 
 const MyHash=new HashTable(50)
-
+//Set the items in the hash table
 MyHash.set('a',1002)
 MyHash.set('b',1003)
 MyHash.set('c',1004)
 MyHash.set('d',1005)
 
-console.log(MyHash.get('b'))
+//console.log(MyHash.get('b'))
+//Simple function to bring out the first recurring number
+
+function itemFirstRepeat(item)
+{
+  const map={}
+  for(let i=0;i<item.length;i++)
+  {
+    if(map[item[i]]!==undefined)
+    {
+      return item[i]
+    }else
+    {
+      map[item[i]]=i
+    }
+  }
+  return map
+}
+
+itemFirstRepeat([1,3,1,4,2,3,4,5,5,6,7,8,34,5])
